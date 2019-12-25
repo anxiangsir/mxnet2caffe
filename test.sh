e2006795c3297bd520224e52dc446188ae35a96b
anxiang <anxiangsir@outlook.com>
2019-12-25 15:21:04 +0800

    add run.sh add test_caffe.py

--- test.sh
+++ test.sh
@@ -1,21 +1,14 @@
 # configs
 mx_model="model"
-mx_json="model-symbol.json"
 epoch=100000
 
 caffe_prototxt="mobileface.prototxt"
 caffe_model="mobileface.caffemodel"
 
-python find_caffe.py
-python find_mxnet.py
 
-# json to prototxt
-python json2prototxt.py \
-  --mx-json $mx_json \
-  --cf-prototxt $caffe_prototxt
 
 # mxnet2caffe
-python mxnet2caffe.py \
+python test_caffe.py \
   --mx-model $mx_model\
   --mx-epoch $epoch \
   --cf-prototxt $caffe_prototxt\
--- test_caffe.py
+++ test_caffe.py
@@ -1,21 +1,46 @@
 import argparse
 
 import caffe
+import mxnet as mx
 import numpy as np
 
 parser = argparse.ArgumentParser(usage="it's usage tip.", description="help info.")
-parser.add_argument('--cf-prototxt', type=str, default='model_caffe/mf.prototxt')
-parser.add_argument('--cf-model', type=str, default='model_caffe/mf.caffemodel')
+parser.add_argument('--mx-model', type=str, default='model_mxnet/mf')
+parser.add_argument('--mx-epoch', type=int, default=1)
 
+parser.add_argument('--cf-prototxt', type=str, default='')
+parser.add_argument('--cf-model', type=str, default='')
 args = parser.parse_args()
-caffe.set_mode_cpu()
-caffe.set_device(0)
-deploy = args.cf_prototxt
-caffe_model = args.cf_model
 
-net = caffe.Net(deploy, caffe_model, caffe.TEST)
-net.blobs['data'].data[...] = np.ones((1, 3, 108, 108))
-out = net.forward()
-pre_fc_caffe = net.blobs['fc1'].data
-output_flatten = np.array(pre_fc_caffe, dtype=np.float32).flatten()
-print(output_flatten.shape)
+
+def main():
+    # input
+    INPUT = np.ones((1, 3, 108, 108))
+
+    # caffe
+    caffe.set_mode_cpu()
+    caffe.set_device(0)
+    deploy = args.cf_prototxt
+    caffe_model = args.cf_model
+
+    net = caffe.Net(deploy, caffe_model, caffe.TEST)
+    net.blobs['data'].data[...] = INPUT
+    out = net.forward()
+    pre_fc_caffe = net.blobs['student_fc'].data
+    output_flatten = np.array(pre_fc_caffe, dtype=np.float32)
+    print(output_flatten.shape)
+
+    # mxnet
+    _, arg_params, aux_params = mx.model.load_checkpoint(args.mx_model, args.mx_epoch)
+    all_layers = _.get_internals()
+    feature = all_layers['student_fc']
+    model = mx.mod.Module(symbol=feature, data_names=['data'], label_names=[])
+
+    model.bind(data_shapes=[('data', (1, 3, 108, 108))],
+               for_training=False, force_rebind=False)
+    model.forward(mx.io.DataBatch([INPUT]))
+    feature = model.get_outputs()[0].asnumpy()
+    print(feature.shape)
+
+if __name__ == '__main__':
+    main()
