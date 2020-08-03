b83adc45a68bd9633b734b31d3335a32bd8d1e5a
anxiangsir <anxiangsir@outlook.com>
2020-08-03 13:51:13 +0800

    update load params

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # configs
 mx_model=/train/trainset/1/modelzoo/FaceFeatureArm_250ms/FaceFeatureArm_250ms
 mx_json="$mx_model".json
-mx_param="$mx_model".param
+mx_param="$mx_model".params
 
 caffe_prototxt="$mx_model".prototxt
 caffe_model="$mx_model".caffemodel
